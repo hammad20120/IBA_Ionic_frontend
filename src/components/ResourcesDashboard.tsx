8135b59ec4b605d9daf725f8960d3390ebfdcb95
Hammad Javed <hammad.javed77@yahoo.com>
2020-09-10 17:51:32 +0500

    Updated Resources Card

--- src/components/ResourcesDashboard.tsx
+++ src/components/ResourcesDashboard.tsx
@@ -11,84 +11,193 @@ import {
   IonIcon,
   IonButton,
   IonSelect,
+  IonGrid,
+  IonCard,
+  IonRippleEffect,
+  IonRow,
+  IonCol,
 } from "@ionic/react";
 import { fastFood, medkit, car, man, bonfire, star } from "ionicons/icons";
 import "./Resources.css";
 
+interface selectedArray {
+  ambulance: boolean;
+  food: boolean;
+  medical: boolean;
+  firetruck: boolean;
+  police: boolean;
+  rescuevehice: boolean;
+}
+
 const ResourcesDashboard: React.FC = () => {
-  const [selected, setSelected] = useState<string>("");
+  const [selected, setSelected] = useState<selectedArray>({
+    ambulance: false,
+    food: false,
+    medical: false,
+    firetruck: false,
+    police: false,
+    rescuevehice: false,
+  });
+
+  const selectHandler = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    name: string,
+    value: boolean
+  ) => {
+    setSelected((prev) => ({
+      ...prev,
+      [name]: !value,
+    }));
+  };
+
   return (
     <div style={{ height: "100%", width: "100%" }} className="ion-padding">
       <IonContent>
-        <h1 style={{ textAlign: "center" }}>Resources</h1>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <h1 className={"cardHeader"}>Resources</h1>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.food ? "selected" : ""
+                  }`}
+                  onClick={(e: any) => selectHandler(e, "food", selected.food)}
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon
+                      icon={fastFood}
+                      className="dashboardicon"
+                    ></IonIcon>
+                    <br />
+                    <br />
+                    Food
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
 
-      {/*  <IonSelect
-          value={selected}
-          multiple={true}
-          onIonChange={(e) => setSelected(e.detail.value)}
-      > */}
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.ambulance ? "selected" : ""
+                  }`}
+                  onClick={(e: any) =>
+                    selectHandler(e, "ambulance", selected.ambulance)
+                  }
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon icon={star} className="dashboardicon"></IonIcon>
+                    <br />
+                    <br />
+                    Ambulance
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
+          </IonRow>
 
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div >
-            <IonIcon icon={fastFood} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Food
-          </div>
-        </IonButton>
-        </IonItem>
+          <IonRow>
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.firetruck ? "selected" : ""
+                  }`}
+                  onClick={(e: any) =>
+                    selectHandler(e, "firetruck", selected.firetruck)
+                  }
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon icon={bonfire} className="dashboardicon"></IonIcon>
+                    <br />
+                    <br />
+                    Fire Truck
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
 
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div>
-            <IonIcon icon={star} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Ambulance
-          </div>
-        </IonButton>
-        </IonItem>
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div>
-            <IonIcon icon={bonfire} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Fire Truck
-          </div>
-        </IonButton>
-        </IonItem>
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div>
-            <IonIcon icon={medkit} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Medical
-          </div>
-        </IonButton>
-        </IonItem>
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div>
-            <IonIcon icon={man} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Rescue Vehicle
-          </div>
-        </IonButton>
-        </IonItem>
-        <IonItem lines="none" style={{float: "right"}}>
-        <IonButton color="buttons" className="dashboardbutton">
-          <div>
-            <IonIcon icon={car} className="dashboardicon"></IonIcon>
-            <br />
-            <br />
-            Police
-          </div>
-        </IonButton>
-        </IonItem>
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.medical ? "selected" : ""
+                  }`}
+                  onClick={(e: any) =>
+                    selectHandler(e, "medical", selected.medical)
+                  }
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon icon={medkit} className="dashboardicon"></IonIcon>
+                    <br />
+                    <br />
+                    Medical
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.rescuevehice ? "selected" : ""
+                  }`}
+                  onClick={(e: any) =>
+                    selectHandler(e, "rescuevehice", selected.rescuevehice)
+                  }
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon icon={man} className="dashboardicon"></IonIcon>
+                    <br />
+                    <br />
+                    Rescue Vehicle
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
+
+            <IonCol>
+              <IonItem lines="none">
+                <IonCard
+                  color="buttons"
+                  className={`dashboardbutton ion-activatable ${
+                    selected.police ? "selected" : ""
+                  }`}
+                  onClick={(e: any) =>
+                    selectHandler(e, "police", selected.police)
+                  }
+                >
+                  <IonRippleEffect></IonRippleEffect>
+                  <div>
+                    <IonIcon icon={car} className="dashboardicon"></IonIcon>
+                    <br />
+                    <br />
+                    Police
+                  </div>
+                </IonCard>
+              </IonItem>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </div>
   );
