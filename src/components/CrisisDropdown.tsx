e2da4c6f359d48d2c911e173217e00eb52ab174f
Hammad Javed <hammad.javed77@yahoo.com>
2020-09-14 17:08:02 +0500

    Some changes

--- src/components/CrisisDropdown.tsx
+++ src/components/CrisisDropdown.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { IonItem, IonLabel, IonSelect, IonSelectOption } from "@ionic/react";
 import "../CSS/CrisisDropDown.css";
